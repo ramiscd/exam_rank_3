0a35e7fe0a48296e48003cc6156394492cfe21da
Ramis Cesar Damasceno <rdamasce@c1r6p4.42sp.org.br>
2026-05-21 19:47:10 -0300

    gnl comentada

--- gnl/broken_gnl.h
+++ gnl/broken_gnl.h
@@ -5,6 +5,12 @@
 #  define BUFFER_SIZE 10
 # endif
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 char	*get_next_line(int fd);
 
 #endif
